b296c1f22f451601e6bdcf0412fdd148d7566914
Maxwell Archibald <maxwellmarchibald@gmail.com>
2024-10-14 21:49:29 -0500

    +Adding GLAB 303.10.1 files

---
+++ module-303/src/labs/l_303_10_1/Boy.java
@@ -0,0 +1,18 @@
+package labs.l_303_10_1;
+
+public class Boy extends Person {
+
+    static double ageFactor = 1.1;
+
+    public String talk() {
+        return (super.talk() + " ... but I love Java class.");
+    }
+
+    public String walk() {
+        return ("I am now walking");
+    }
+
+    public static double lifeSpan() {
+        return (lifeSpan * ageFactor);
+    }
+}
